8fef75de3b89ead649419a4201bc94c7726019a0
Rodrigo <PachecoRodrigo97@hotmail.com>
2021-02-27 14:13:23 -0300

    first commit

--- src/hooks/getWeather.js
+++ src/hooks/getWeather.js
@@ -1,7 +1,7 @@
 
 export const getWeather = async (city) => {
 
-    const resp = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=6ea654de70b97198ff373da06eeef738`);
+    const resp = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=6ea654de70b97198ff373da06eeef738`);
     const data = await resp.json();
 
     if (data.cod === "404") {
